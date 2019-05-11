bee7ef005ea5216a7ced36bf6bec96de54cf2a26
asb <ipritoflex@yandex.ru>
2019-05-11 18:29:31 +0300

    Update

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,12 +1,15 @@
 import { SocketManager } from '../utils/network/socket-manager';
 import { SOCKET_URL } from '../utils/misc';
 
-export default function ({ app, store }) {
+export default function ({ app, store }, inject) {
   if (typeof window === 'undefined') {
     return;
   }
 
   const socketManager = SocketManager.getManager();
+
+  inject( 'sm', socketManager );
+
   return socketManager.connect( SOCKET_URL ).then(_ => {
     const socket = socketManager.socket;
     socket.on('issues.update', issue => {
