3fd731b98e0483d3c166e59820acc0a6c08ca01a
asb <ipritoflex@yandex.ru>
2019-05-11 04:33:05 +0300

    Update

---
+++ src/pages/awards.vue
@@ -0,0 +1,31 @@
+<script>
+  import JsMoneyTable from '../components/common/money/JsMoneyTable';
+
+  export default {
+    name: 'awards-page',
+
+    layout: 'default',
+
+    components: {
+      JsMoneyTable
+    },
+
+    async fetch ({ app, store, route }) {
+      const { dispatch } = store;
+
+      return Promise.all([
+        dispatch( 'money/fetchTable' ),
+      ]);
+    }
+  };
+</script>
+
+<template>
+  <div class="awards-page">
+    <js-money-table class="awards-page__table"></js-money-table>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "../styles/pages/awards-page";
+</style>
